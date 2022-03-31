c97fc4dc8b2470a959314e0522b4317b9c3e2614
RevDonz <aprilio842@gmail.com>
2022-03-31 22:27:02 +0700

    Ini commit

---
+++ KuliahMahasiswa1302204051.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_kelompok_04
+{
+    internal class KuliahMahasiswa1302204051
+    {
+        public string kode_matkul { get; set; }
+        public string nama_matkul { get; set; }
+
+        public KuliahMahasiswa1302204051(string kode_matkul, string nama_matkul)
+        {
+            this.kode_matkul = kode_matkul;
+            this.nama_matkul = nama_matkul;
+        }
+        
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace tpmodul6_kelompok_04;
@@ -10,8 +11,16 @@ internal class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Halo Dunia");
-        Console.WriteLine("Zahrandi tamvan");
-        Console.WriteLine("Anyeongggggg");
+        //Console.WriteLine("Halo Dunia");
+        //Console.WriteLine("Zahrandi tamvan");
+        //Console.WriteLine("Anyeongggggg");
+
+
+        String jsonString = File.ReadAllText("E://SEMESTER 4/Konstruksi Perangkat Lunak/Praktikum/Modul 5/tpmodul6_kelompok_04/tp6_2_nim.json");
+
+        KuliahMahasiswa1302204051 mhs1 = JsonSerializer.Deserialize<KuliahMahasiswa1302204051>(jsonString);
+
+        //Console.WriteLine("Kode Mata Kuliah : " + mhs1.kode_matkul);
+        //Console.WriteLine("Nama mata Kuliah : " + mhs1.nama_matkul);
     }
 }
