5a711c98dc80be1e69bb7631766241093f9fa784
RevDonz <aprilio842@gmail.com>
2022-03-31 21:43:49 +0700

    Menambah Main Class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_kelompok_04;
+
+internal class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Halo Dunia");
+    }
+}
